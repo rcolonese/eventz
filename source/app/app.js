d62663738cbdbdee0b9057572aa9f74aca4d1a9c
Raphael J Colonese <rcolonese@gmail.com>
2016-05-21 09:36:36 -0300

    Corrigida a  injeção de dependência Eventz.config para Eventz.Main

--- source/app/app.js
+++ source/app/app.js
@@ -4,7 +4,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'Eventz' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('Eventz', ['ionic', 'ngResource', 'Eventz.config', 'Eventz.Events', 'Eventz.Users'])
+angular.module('Eventz', ['ionic', 'ngResource', 'Eventz.Main', 'Eventz.Events', 'Eventz.Users'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
